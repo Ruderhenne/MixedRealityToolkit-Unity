458ff0b1c022eb1227484ae89e4cba81799502d0
Ruderhenne <ruderhenne@web.de>
2026-02-12 11:41:17 +0100

    MultiUserScene eingerichtet
    - DashboardPanel ist nun ein Prefab (Ordner: Assets -> Resources)
    - an MRTK XR Rig und DashboardPanel Prefab hängen nun Photon View Componenten, die für die Synchronisierung sorgen
    - das Skript RoomManager sorgt für das Spawnen der Dashboards
    
    Derzeit findet noch KEINE Synchronisierung der Werte auf dem Dashboard statt!
    Zwei Instanzen können aber gestartet werden.

---
+++ UnityProjects/MRTKDevTemplate/Assets/Scripts/MyScripts/RoomManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        Debug.Log("RoomManager Start");
+        PhotonNetwork.ConnectUsingSettings();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Photon Master");
+        RoomOptions roomOptions = new RoomOptions { MaxPlayers = 4 };
+        PhotonNetwork.JoinOrCreateRoom("DashboardRoom", roomOptions, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log($"Joined Room: {PhotonNetwork.CurrentRoom.Name}");
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Transform cam = Camera.main.transform;
+            Vector3 pos = cam.position + cam.forward * 0.5f;
+            Quaternion rot = Quaternion.LookRotation(cam.forward);
+
+            GameObject dash = PhotonNetwork.Instantiate("DashboardPanel", pos, rot);
+            Debug.Log($"Dashboard at {pos} - Children: {dash.transform.childCount}");
+        }
+    }
+
+}
